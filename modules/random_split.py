84577ec6a40ef41ba30184a6c018e66174733cdf
kevin <kevin.jerusalmi@fairgen.ai>
2025-03-04 11:16:14 +0200

    remove baseline

--- modules/random_split.py
+++ modules/random_split.py
@@ -10,8 +10,8 @@ def app():
 
     if "remove_baseline_from_holdout" not in st.session_state:
         st.session_state["remove_baseline_from_holdout"] = True
-    if "with_baseline" not in st.session_state:
-        st.session_state["with_baseline"] = True
+    # if "with_baseline" not in st.session_state:
+    #     st.session_state["with_baseline"] = True
     if "boostrap" not in st.session_state:
         st.session_state["boostrap"] = False
     if "boostrap_occurences" not in st.session_state:
@@ -40,28 +40,28 @@ def app():
             )
 
         train_size = int((train_size_percentage / 100) * total_size)
-        holdout_size = total_size - train_size if not st.session_state["remove_baseline_from_holdout"] else total_size - (2 * train_size)
-        holdout_size_percentage = 100 - train_size_percentage * 2 if st.session_state["remove_baseline_from_holdout"] else 100 - train_size_percentage
+        holdout_size = total_size - train_size #  if not st.session_state["remove_baseline_from_holdout"] else total_size - (2 * train_size)
+        holdout_size_percentage = 100 - train_size_percentage * 2 # if st.session_state["remove_baseline_from_holdout"] else 100 - train_size_percentage
 
         with col_stats:
             st.metric(label="Total Size", value=f"{total_size} rows")
             st.metric(label=f"Train Size ({train_size_percentage}%)", value=f"{train_size} rows")
             st.metric(label=f"Holdout Size ({holdout_size_percentage}%)", value=f"{holdout_size} rows")
-            if st.session_state["with_baseline"]:
-                st.metric(label=f"Baseline Size ({train_size_percentage*2}%)", value=f"{train_size*2} rows")
+            # if st.session_state["with_baseline"]:
+            #     st.metric(label=f"Baseline Size ({train_size_percentage*2}%)", value=f"{train_size*2} rows")
 
-        new_baseline_state = st.toggle("Baseline", value=st.session_state["with_baseline"])
-        if new_baseline_state != st.session_state["with_baseline"]:
-            st.session_state["with_baseline"] = new_baseline_state
-            if not new_baseline_state:
-                st.session_state["remove_baseline_from_holdout"] = False  # Force remove_baseline to be False
-            st.rerun()
+        # new_baseline_state = st.toggle("Baseline", value=st.session_state["with_baseline"])
+        # if new_baseline_state != st.session_state["with_baseline"]:
+        #     st.session_state["with_baseline"] = new_baseline_state
+        #     if not new_baseline_state:
+        #         st.session_state["remove_baseline_from_holdout"] = False  # Force remove_baseline to be False
+        #     st.rerun()
 
-        if st.session_state["with_baseline"]:
-            remove_baseline_from_holdout = st.toggle("Remove Baseline from holdout", value=st.session_state["remove_baseline_from_holdout"])
-            if remove_baseline_from_holdout != st.session_state["remove_baseline_from_holdout"]:
-                st.session_state["remove_baseline_from_holdout"] = remove_baseline_from_holdout
-                st.rerun()
+        # if st.session_state["with_baseline"]:
+        #     remove_baseline_from_holdout = st.toggle("Remove Baseline from holdout", value=st.session_state["remove_baseline_from_holdout"])
+        #     if remove_baseline_from_holdout != st.session_state["remove_baseline_from_holdout"]:
+        #         st.session_state["remove_baseline_from_holdout"] = remove_baseline_from_holdout
+        #         st.rerun()
 
         new_boostrap_state = st.toggle("Boostrap", value=st.session_state["boostrap"])
         if new_boostrap_state != st.session_state["boostrap"]:
@@ -89,8 +89,8 @@ def app():
         split_results = split_utils.random_split(
             st.session_state["data"], 
             train_size=train_size_percentage / 100, 
-            baseline=st.session_state["with_baseline"], 
-            remove_baseline=st.session_state["remove_baseline_from_holdout"], 
+            baseline=False  # st.session_state["with_baseline"], 
+            remove_baseline=False  # st.session_state["remove_baseline_from_holdout"], 
             random_states=random_states
         )
 
@@ -101,8 +101,8 @@ def app():
             files_utils.save_file(df=train_df, metadata=meta, file_path=f"outputs/train_{train_size}{suffix}" + "." +  st.session_state["file_type"])
             files_utils.save_file(df=holdout_df, metadata=meta, file_path=f"outputs/holdout_{holdout_size}{suffix}" + "." + st.session_state["file_type"])
 
-            if st.session_state["with_baseline"]:
-                files_utils.save_file(df=baseline_df, metadata=meta, file_path=f"outputs/baseline_{train_size*2}{suffix}" + "." + st.session_state["file_type"])
+            # if st.session_state["with_baseline"]:
+            #     files_utils.save_file(df=baseline_df, metadata=meta, file_path=f"outputs/baseline_{train_size*2}{suffix}" + "." + st.session_state["file_type"])
 
         st.success("Data has been successfully split!")
 
